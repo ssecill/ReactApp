a3de1aba5e4bbf4e38ab8da33a8fc10c0d088bd9
secil <secilnurr@hotmail.com>
2021-05-19 14:32:07 +0300

    react

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from "react";
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams
+} from "react-router-dom";
+
+export default function ParamsExample() {
+  return (
+    <Router>
+      <div>
+            <Link to="/bcfm"></Link>
+        <Switch>
+          <Route path="/:id" children={<Child />} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Child() {
+  // We can use the `useParams` hook here to access
+  // the dynamic pieces of the URL.
+  let { id } = useParams();
+
+  return (
+   <div className="App">
+      <header className="App-header">
+    <div>
+      <h3>SECIL</h3>
+    </div>
+	      </header>
+	 </div>
+  );
+}
